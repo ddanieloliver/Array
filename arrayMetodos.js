83532b0415b9982a70dcda6528db9de87841c596
ddanieloliver <dnielvitoroliveira456@gmail.com>
2022-02-14 16:38:04 -0300

    Métodos muito comuns e importantes de saber

--- arrayMetodos.js
+++ arrayMetodos.js
@@ -14,17 +14,17 @@ console.log(pilotos)
 //splice pode adicionar e remover elementos
 
 //adicionar
-pilotos.splice(2,0, 'Bottas', 'Massa')
+pilotos.splice(2, 0, 'Bottas', 'Massa')
 console.log(pilotos)
 //
 
 //remover
-pilotos.splice(3,1) // remove o um elemento a partir do indice 3, nesse caso, o proprio 3
+pilotos.splice(3, 1) // remove o um elemento a partir do indice 3, nesse caso, o proprio 3
 console.log(pilotos)
 //
 
 const algunsPilotos1 = pilotos.slice(2)// gera um novo array a partir do indice selecionado do antigo
 console.log(algunsPilotos1)
 
-const algunsPilotos2 = pilotos.slice(1,4) // aqui també gera um novo array, mas o indice 1 entra, o 4 não.
+const algunsPilotos2 = pilotos.slice(1, 4) // aqui també gera um novo array, mas o indice 1 entra, o 4 não.
 console.log(algunsPilotos2)
